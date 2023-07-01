5ee9ca1185101279bf8e8f69c6bb7e7c1fc6ae07
shojanob <shojanob03@gmail.com>
2023-07-01 15:43:09 -0400

    Version 12

--- version1.java
+++ version1.java
@@ -1,7 +1,7 @@
 public class version1
 {
     public static void main(String[] args) {
-        System.out.println("This is our first line of Java code!");
+        System.out.println("This is our 12th line of Java code!");
         int[] nums = {2, 5, 18, 12, 2};
     }
 
--- version2.java
+++ version2.java
@@ -1,6 +1,6 @@
 public class version2
 {
     public static void main(String[] args) {
-        System.out.println("This is our second Java git version!");
+        System.out.println("This is our 12th Java git version!");
     }
 }

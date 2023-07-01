72d3382b22f67b6cb4f4d29d005a1dc4ba78ee28
shojanob <shojanob03@gmail.com>
2023-07-01 00:06:56 -0400

    Version2

--- version1.java
+++ version1.java
@@ -3,4 +3,6 @@ public class version1
     public static void main(String[] args) {
         System.out.println("This is our first line of Java code!");
     }
+
+    
 }
---
+++ version2.java
@@ -0,0 +1,6 @@
+public class version2
+{
+    public static void main(String[] args) {
+        System.out.println("This is our second Java git version!");
+    }
+}

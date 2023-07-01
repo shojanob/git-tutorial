3737b033f77bc8a7e219e4e434e69e56b9254220
shojanob <shojanob03@gmail.com>
2023-07-01 00:31:04 -0400

    Version 2 restored

--- version2.java
+++ version2.java
@@ -2,7 +2,5 @@ public class version2
 {
     public static void main(String[] args) {
         System.out.println("This is our second Java git version!");
-
-        String[] students = {"Jim", "Jenny", "Lucas"};
     }
 }
